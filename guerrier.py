1978a88e3bb0bc1ce0b5da465fdd6068f1a31f1a
python <thomas.win@gmail.com>
2019-12-16 15:18:26 +0100

    creation classe et class mère

---
+++ guerrier.py
@@ -0,0 +1,3 @@
+class Guerrier(Personnage):
+    def __init__(self, attaque, défense, agilité, nom):
+        super().__init__(100, 50, 10, nom)
---
+++ personnage.py
@@ -0,0 +1,13 @@
+"""class mère project rpg"""
+
+class Personnage:
+    
+    def __init__(self, attaque, défense, agilité, nom, mana):
+        self.attaque = attaque
+        self.défense = défense
+        self.agilité = agilité
+        self.nom = nom
+        self.mana = mana
+
+
+
