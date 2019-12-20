773729165a5b745a49d4f6e15301376ad2acb620
El Habib <Abdelaziz.59140@gmail.com>
2019-12-20 12:25:19 +0100

    modification method

--- game/game.py
+++ game/game.py
@@ -10,66 +10,74 @@ class Game:
         self.nom = None
         self.perso = None
         self.ennemy = None
-        self.attack = None
+        
         
     def player_name(self):
         partie1()
         self.nom= input("Quel est votre nom ? ").upper()
-        print(f"Bienvenue {self.nom}, jeune padawan de la programmation !\n")
+        print(f"Bienvenue {self.nom}, il est temps!\n")
 
     def character_choice(self):
         print("[GUERRIER] |[ARCHER] | [MAGICIEN]\n")
-        self.perso = input("Choisissez un aventurier parmis ces trois personnage :\n").lower()
+        self.perso = input("Choisissez un aventurier parmis ces trois personnage :" ).lower()
         if self.perso == "guerrier":
-            return Guerrier()
+            self.perso = Guerrier()
+            return self.perso
         elif self.perso == "archer":
-            return Archer()
+            self.perso = Archer()
+            return self.perso
         elif self.perso == "magicien":
-            return Magicien()
+            self.perso = Magicien()
+            return self.perso
         else:
-            print("je connais pas cette aventurier !")
+            print("Désoler, je connais pas cette aventurier !")
             self.character_choice()
             
     def ennemy_choice(self):
         self.ennemy = Thomas_le_méchant()
-        print("Thommas le méchant s'approche de vous armé de sa grande régles épineuse !\n")
+        print("THOMMAS LE MÉCHANT ;-( S'APPROCHE DE VOUS ARMÉ DE SA GRANDE RÉGLES ÉPINEUSE !\n--DEUX CHOIX S'OFFRE À VOUS-- ")
         return self.ennemy
 
-    def ennemy_vs_player(self, perso, ennemy):
+    def ennemy_vs_player(self):
         self.character_choice()
         self.ennemy_choice()
-        while perso.vie > 0 and ennemy.vie > 0:
+        while self.perso.vie > 0 and self.ennemy.vie > 0:
             print("[Attaque: A , Fuir: F]")
-            action = input("Que choisissez-vous faire ? ").lower()
+            action = input("Que choisissez-vous de faire ? ").lower()
             if action == "a":
-                self.fight_action(perso, ennemy)
-                print(f"{self.perso} attaque violamment {self.ennemy.name}")
-                print(f"[votre vie est de {perso.vie} points]")
-                print(f"[votre défense est de {perso.défense} points]\n")  
+                self.fight_action()
+                print(f"{self.nom} | attaque violamment | {self.ennemy.name}\n")
+                print(f"[votre vie est de {self.perso.vie} points]")
+                print(f"[votre défense est de {self.perso.défense} points]\n") 
+                print(f"[la vie de {self.ennemy.name} est de {self.ennemy.vie} points]")
+                print(f"[la défense de Thomas_le_méchant est de {self.ennemy.défense} points]\n")   
             else:
-                print("fuite")
-                self.fight_action(ennemy, perso)
-                print(ennemy.vie)
+                action == "f"
+                self.fight_action()
+                print(f"{self.nom} | tente de fuir | {self.ennemy.name}\n")
+                print(f"[la vie de {self.ennemy.name} est de {self.ennemy.vie} points]")
+                print(f"[la défense de Thomas_le_méchant est de {self.ennemy.défense} points]\n")
+                print(f"[votre vie est de {self.perso.vie} points]")
+                print(f"[votre défense est de {self.perso.défense} points]\n") 
+
         print("le combat est terminé !")
         
 
-
-    def fight_action(self, ennemy, character):
-
-        if ennemy.défense > 0:
-            if ennemy.défense < character.attaque:
-                ennemy.vie -= (character.attaque - ennemy.défense)
-                ennemy.défense = 0
+    def fight_action(self):
+        if self.ennemy.défense > 0:
+            if self.ennemy.défense < self.perso.attaque:
+                self.ennemy.vie -= (self.perso.attaque - self.ennemy.défense)
+                self.ennemy.défense = 0
             else:
-                ennemy.défense -= character.attaque
+                self.ennemy.défense -= self.perso.attaque
         else:
-            if ennemy.défense == 0:
-                ennemy.vie -= character.attaque
-                if ennemy.vie <= 0:
-                    ennemy.vie = 0
+            if self.ennemy.défense == 0:
+                self.ennemy.vie -= self.perso.attaque
+                if self.ennemy.vie <= 0:
+                    self.ennemy.vie = 0
             else:
-                ennemy.vie -= (character.attaque - ennemy.défense)
-        return(ennemy.vie)
+                self.ennemy.vie -= (self.perso.attaque - self.ennemy.défense)
+        return(self.ennemy.vie)
 
 
 
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ if __name__ == "__main__":
     
     g = Game()
     g.player_name()
-    #print(g.player_name)
-    ennemy = g.ennemy_choice()
-    character = g.character_choice()
-    #print(ennemy,character)
-    g.ennemy_vs_player(ennemy, character)
+    #ennemy = g.ennemy_choice()
+    #perso = g.character_choice()
+    g.ennemy_vs_player()
     
     
     
